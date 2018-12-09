18434c1a73588fa7b89db55e961d6cb7bc86cff1
unknown <santosh.yadav198613@gmail.com>
2018-12-09 12:41:07 +0530

    added formArray controls

--- src/app/employee/employee-onboarding/employee-onboarding.component.ts
+++ src/app/employee/employee-onboarding/employee-onboarding.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-onboarding',
@@ -18,16 +18,36 @@ export class EmployeeOnboardingComponent implements OnInit {
       lastName: new FormControl(''),
       email: new FormControl(''),
       dob: new FormControl(''),
-      address : this.fb.group({
+      address: this.fb.group({
         addressLine1: new FormControl(''),
         addressLine2: new FormControl(''),
         city: new FormControl(''),
         pin: new FormControl('')
-      })
+      }),
+      experience: this.fb.array([
+        this.buildForm()
+      ])
     }
     );
   }
 
+  buildForm() {
+    return this.fb.group({
+      employer: new FormControl(''),
+      startDate: new FormControl(''),
+      endDate: new FormControl(''),
+      position: new FormControl('')
+    });
+  }
 
+  addEmployer() {
+    const experience = this.onboardingForm.controls['experience'] as FormArray;
+    experience.push(this.buildForm());
+  }
+
+  removeEmployer(i:number) {
+    const experience = this.onboardingForm.controls['experience'] as FormArray;
+    experience.removeAt(i);
+  }
 
 }
