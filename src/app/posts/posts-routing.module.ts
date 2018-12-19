b13b173826c06f2556cf4cd583f1a611fe7043f5
unknown <santosh.yadav198613@gmail.com>
2018-12-19 11:43:49 +0530

    added dynamic routing

--- src/app/posts/posts-routing.module.ts
+++ src/app/posts/posts-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PostsComponent } from './posts.component';
+import { PostAddComponent } from './post-add/post-add.component';
 
 const routes: Routes = [
-  { path: 'posts', component: PostsComponent }
+  { path: 'posts', component: PostsComponent },
+  { path: 'posts/:id' , component : PostAddComponent }
 ];
 
 @NgModule({
