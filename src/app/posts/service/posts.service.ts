eec1e8577cb82794d84084b356c1c10d42a58ae3
unknown <santosh.yadav198613@gmail.com>
2018-12-28 15:21:07 +0530

    added configuration in environment files

--- src/app/posts/service/posts.service.ts
+++ src/app/posts/service/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Iposts } from './Iposts';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -13,21 +14,21 @@ export class PostsService {
     // return this.http.get<Iposts[]>('https://jsonplaceholder.typicode.com/posts',
     //   { headers: new HttpHeaders().set('apikey', 'dsdsfgdg') }); //this is using for Get Method all Data Displaing
 
-    return this.http.get<Iposts[]>('https://jsonplaceholder.typicode.com/posts');
+    return this.http.get<Iposts[]>(environment.apiEndpoint + 'posts');
   }
 
   addPost(post: Iposts) {
     // return this.http.post<Iposts>('https://jsonplaceholder.typicode.com/posts', post,
     //  { headers: new HttpHeaders().set('apikey', 'dsdsfgdg') });
 
-    return this.http.post<Iposts>('https://jsonplaceholder.typicode.com/posts', post);
+    return this.http.post<Iposts>(environment.apiEndpoint + 'posts', post);
   }
 
   getPostById(id: number) {
-    return this.http.get<Iposts>('https://jsonplaceholder.typicode.com/posts/' + id);
+    return this.http.get<Iposts>(environment.apiEndpoint + 'posts/' + id);
   }
 
   updatePostById(post: Iposts) {
-    return this.http.put<Iposts>('https://jsonplaceholder.typicode.com/posts/' + post.id, post);
+    return this.http.put<Iposts>(environment.apiEndpoint + 'posts/' + post.id, post);
   }
 }
